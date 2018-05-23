dc837b63fe5c21810898b2563ba52bc3491a15ab
gabymq <gmorenogaby@gmail.com>
2018-05-23 14:15:23 -0500

    Fix #6

--- models/user.js
+++ models/user.js
@@ -1,13 +1,49 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
-    userName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    phone: DataTypes.STRING
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      type: DataTypes.UUID,
+    },
+    userName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      required: true,
+    },
+    lastName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      required: true,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
+      required: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      required: true,
+      validate: {
+        len: [7, 10],
+      }
+    },
+    phone: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      required: true,
+      validate:{
+        len: [10, 15],
+      }
+    },
   }, {});
-  User.associate = function(models) {
+  User.associate = function (models) {
     // associations can be defined here
   };
   return User;
